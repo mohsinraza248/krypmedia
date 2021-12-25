4f09d119b1947d3406c421b89bf5448a6c30a14c
Mohsin Raza <84380166+mohsinraza248@users.noreply.github.com>
2021-12-25 12:14:56 +0530

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,9 +3,12 @@
 							<li><a href="Holovsn">HOLOVSN</a></li>
 							<li><a href="Holomist">HOLOMIST</a></li>
 							<li><a href="Digiplay">DIGIPLAY</a></li>
-					                
-							<li><a href="Holopyramid">HOLOPYRAMID</a></li>
+						        <li><a href="AR">DIGITAL SOLUTIONS</a></li>
+						        <li><a href="Holonet">HOLONET</a></li>
+						        <li><a href="Holopyramid">HOLOPYRAMID</a></li>
 							<li><a href="Xhibit">VIRTUAL CONFERENCE</a></li>
+						        <li><a href="Blacklight">BLACKLIGHT</a></li>
+						        <li><a href="Liveglass">LIVEGLASS</a></li>
 							
 						</ul>
       				</div>
