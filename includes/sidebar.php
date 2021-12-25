0b47cd81f7e205c994b716b90de11bf53a3b5775
Mohsin Raza <84380166+mohsinraza248@users.noreply.github.com>
2021-12-25 11:56:18 +0530

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,10 +3,10 @@
 							<li><a href="Holovsn">HOLOVSN</a></li>
 							<li><a href="Holomist">HOLOMIST</a></li>
 							<li><a href="Digiplay">DIGIPLAY</a></li>
-							<li><a href="Digiposter">DIGIPOSTER</a></li>
+					                <!--<li><a href="Digiposter">DIGIPOSTER</a></li>--!>
 							<!--<li><a href="AR">AR VR</a></li>-->
 						<!--	<li><a href="VR">VR</a></li> -->
-							<li><a href="NeuroFocus">AI</a></li>
+						<!--	<li><a href="NeuroFocus">AI</a></li>  --!>
 							<li><a href="Liveglass">LIVEGLASS</a></li>
 							<li><a href="Blacklight">BLACKLIGHT</a></li>
 							<!--<li><a href="Hydravsn">HYDRAVSN</a></li>-->
