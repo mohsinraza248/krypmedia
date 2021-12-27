bce5b2c058a1ba7c8fa834b62828e7df6e757452
Mohsin Raza <84380166+mohsinraza248@users.noreply.github.com>
2021-12-27 17:26:57 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -45,7 +45,7 @@
 				</div>
 			</div>
 			<div class="col-sm-3">
-				<div class="tab" style="margin-top: 30px;">
+				<div class="tab exp">
 					<ul>
 					    <li><a href="Holopyramid"><i class="fas fa-chevron-right"></i>Holopyramid</a></li>
 						<li><a href="Digiplay"><i class="fas fa-chevron-right"></i>Digiplay</a></li>
