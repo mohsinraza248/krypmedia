5d98ce6a0eee118a127acd53e884232a45243542
Mohsin Raza <84380166+mohsinraza248@users.noreply.github.com>
2021-12-28 11:43:02 +0530

    Update footer-script.php

--- includes/footer-script.php
+++ includes/footer-script.php
@@ -26,21 +26,21 @@
 		margin: 10,
 		loop: true,
 		nav:true,
-		navText: ["Prev","Next"]
-		responsiveClass:true,
+		navText: ["Prev","Next"],
+		dots:false,
 		responsive:{
 			0:{
 				items:1,
-				nav:true,
+				
 			},
 			600:{
 				items:3,
-				nav: true,
+				
 			},
 			1000:{
 				items:3,
-				nav:true,
-				loop:false,
+				
+				
 			}
 		}
 	})
